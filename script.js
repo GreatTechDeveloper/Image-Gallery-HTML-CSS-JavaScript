d73963ea79b478828202382cd18bbe8d0e224e11
mohammad shakiba <mohammmad1069@gmail.com>
2024-08-17 08:20:32 +0330

    First Commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const scrollContainer = document.querySelector(".gallery");
+const backBtn = document.getElementById("backBtn");
+const nextBtn = document.getElementById("nextBtn");
+
+scrollContainer.addEventListener("wheel", (event) => {
+  event.preventDefault();
+  scrollContainer.scrollLeft -= event.deltaY;
+  scrollContainer.style.scrollBehavior = "smooth";
+});
+
+nextBtn.addEventListener("click", () => {
+  scrollContainer.style.scrollBehavior = "smooth";
+  scrollContainer.scrollLeft += 900;
+});
+
+backBtn.addEventListener("click", () => {
+    scrollContainer.style.scrollBehavior = "smooth";
+  scrollContainer.scrollLeft -= 900;
+});
